e57c4c8bd061db5f3e301705eee3e0de1f9bfc1a
Francisco R. M. Porfírio <80773101+f-rxfxel@users.noreply.github.com>
2023-02-14 22:12:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+window.onload = () => {
+   /*  alert("passo 1")
+    alert("passo 2")*/
+    if(JSON.parse(localStorage.getItem('showTutorial'))) {
+        if (confirm("deseja ver os passos novamente?")) {
+            alert("mostraremos os passos");
+            localStorage.setItem('showTutorial', JSON.stringify(true));
+        } else {
+            alert("passos não serão exibidos")
+            localStorage.setItem('showTutorial', JSON.stringify(false));
+        }
+    }
+}
+
 const saveBtn = document.getElementById('saveBtn')
 const taskInput = document.getElementById('taskInput')
 const tasks = document.getElementById('tasks')
-saveBtn.addEventListener("click", ()=>{
+
+saveBtn.addEventListener("click", () => {
     addTask()
+})
+
+saveBtn.addEventListener("dblclick", () => {
+    alert("dbc")
 })
 
 function addTask() {
@@ -11,4 +30,18 @@ function addTask() {
     div.innerText = taskName
     tasks.appendChild(div);
     taskInput.value = ""
+
+    div.addEventListener("dblclick", () => {
+        div.innerText = ""
+    })
+
+    div.style.backgroundColor = "rgb(255, 255, 255)"
+
+    div.addEventListener("click", () => {
+        if(div.style.backgroundColor == "rgb(255, 255, 255)") {
+            div.style.backgroundColor = "#00FF00";
+        } else {
+            div.style.backgroundColor = "#FFFFFF";
+        }
+    })
 }

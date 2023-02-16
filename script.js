87db3978f190ece4d042c3d6af226491c675baaf
Francisco R. M. Porfírio <80773101+f-rxfxel@users.noreply.github.com>
2023-02-15 22:02:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 window.onload = () => {
-   /*  alert("passo 1")
-    alert("passo 2")*/
-    if(JSON.parse(localStorage.getItem('showTutorial'))) {
-        if (confirm("deseja ver os passos novamente?")) {
-            alert("mostraremos os passos");
-            localStorage.setItem('showTutorial', JSON.stringify(true));
-        } else {
-            alert("passos não serão exibidos")
-            localStorage.setItem('showTutorial', JSON.stringify(false));
+    // alert("passo 1")
+    // alert("passo 2")
+
+    if (JSON.parse(localStorage.getItem('showTutorial')) == undefined) {
+        localStorage.setItem('showTutorial', JSON.stringify(true))
+        showSteps()
+    } else {
+        showSteps()
+    }
+
+    function showSteps() {
+        if (JSON.parse(localStorage.getItem('showTutorial'))) {
+            alert("passo 1")
+            alert("passo 2")
+            verifyTutorial()
         }
+    }
+
+}
+
+function verifyTutorial() {
+    if (confirm("deseja ver os passos novamente?")) {
+        alert("mostraremos os passos");
+        localStorage.setItem('showTutorial', JSON.stringify(true));
+    } else {
+        alert("passos não serão exibidos")
+        localStorage.setItem('showTutorial', JSON.stringify(false));
     }
 }
 
 const saveBtn = document.getElementById('saveBtn')
 const taskInput = document.getElementById('taskInput')
 const tasks = document.getElementById('tasks')
+const teste = document.getElementById('teste')
+
+teste.addEventListener("click", () => {
+    alert("passo 1")
+    alert("passo 2")
+    verifyTutorial()
+})
 
 saveBtn.addEventListener("click", () => {
     addTask()
@@ -38,7 +62,7 @@ function addTask() {
     div.style.backgroundColor = "rgb(255, 255, 255)"
 
     div.addEventListener("click", () => {
-        if(div.style.backgroundColor == "rgb(255, 255, 255)") {
+        if (div.style.backgroundColor == "rgb(255, 255, 255)") {
             div.style.backgroundColor = "#00FF00";
         } else {
             div.style.backgroundColor = "#FFFFFF";
